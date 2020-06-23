fa075b6d9699fae84e506ad69f7bfff2f736ea82
laugeeme <registro.laura@gmail.com>
2020-06-23 16:48:49 +0200

    define interface selectors

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let DB;
+
+//Interface selectors
+const form = document.querySelector('form'),
+  petName = document.querySelector('#mascota'),
+  clientName = document.querySelector('#cliente'),
+  phone = document.querySelector('#telefono'),
+  date = document.querySelector('#fecha'),
+  hour = document.querySelector('#hora'),
+  symptoms = document.querySelector('#sintomas'),
+  appointments = document.querySelector('#citas'),
+  adminHeading = document.querySelector('#administra');
