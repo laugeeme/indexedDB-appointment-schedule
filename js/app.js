38be85b729d3c1c05bc60aac5e4a54afc4f08dbf
laugeeme <registro.laura@gmail.com>
2020-06-23 17:06:29 +0200

    create data base, schema, for the fist field of the form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict';
 
 let DB;
@@ -12,3 +13,34 @@ const form = document.querySelector('form'),
   symptoms = document.querySelector('#sintomas'),
   appointments = document.querySelector('#citas'),
   adminHeading = document.querySelector('#administra');
+
+//Wait for DOM Ready
+document.addEventListener('DOMContentLoaded', () => {
+  //create data base (2 parameters name of data base and version of data base)
+  let createDB = window.indexedDB.open('citas', 1);
+
+  //if there is error send to console
+  createDB.onerror = function () {
+    console.log('Hubo un error');
+  };
+  //if it's ok show in console and asign to data base
+  createDB.onsuccess = function () {
+    //asign to data base
+    DB = createDB.result;
+  };
+
+  //create schema, it only runs once
+  createDB.onupgradeneeded = function (e) {
+    //event is the data base
+    let db = e.target.result;
+
+    //defines objectstore. 2 parameters, name of the data base and options. KeyPath its the index of the data base
+    let objectStore = db.createObjectStore('citas', {
+      keyPath: 'key',
+      autoIncrement: true,
+    });
+
+    //create index and fields of the data base. 3 parameters, name, keypath and options
+    objectStore.createIndex('mascota', 'mascota', { unique: false});
+  };
+});
